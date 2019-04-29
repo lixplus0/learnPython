6cebe4d90ca6621c859d5d7a61f1f428ca9e2f2a
lixplus <lixplus@qq.com>
2019-04-29 23:56:17 +0800

    2019-04-29 23:56:14

--- Research/endnote_to_bibtex.py
+++ Research/endnote_to_bibtex.py
@@ -3,16 +3,17 @@ import re
 def solve_title(_first_line):
     reg1 = '@[a-zA-Z]+{'
     line_title = re.match(reg1, _first_line).group()
-    reg2 = '{[a-zA-Z],'
-    author_first = re.search(reg2, _first_line).group()[1:-2]
-    year = _first_line.split('#')[-1]
+    reg2 = '{.+?,'
+    author_first = re.search(reg2, _first_line).group()[1:-1]
+    year = _first_line.split('#')[-1].split('\\')[0]
     return line_title + author_first + year
 
 def main():
-    file_path = r"C:\Users\lixplus\Desktop\LxWork.txt"
+    file_path = r"D:\Research\LxWork.txt"
     with open(file_path, 'r', encoding='UTF-8-sig') as data_file:
         for line in data_file.readlines():
-            solve_title(line)
+            if line[0] == '@':
+                print(solve_title(line))
 
 if __name__ == '__main__':
     main()
