24f09882c9362931345ad647cb5e4a218e3cee21
lixplus <lixplus@qq.com>
2019-04-29 16:17:35 +0800

    2019-04-29 16:17:33

---
+++ Video_Pic/video2audio.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+from natsort import natsorted
+
+ffmpeg = r'"D:\Program Files (x86)\MarukoToolbox\tools\ffmpeg.exe"'
+video_path = r"D:\MyFiles\Downloads\Video\相声小品全集\已筛选小品"
+
+for root, dirs, files in os.walk(video_path):
+    # 按文件名排序
+    files = natsorted(files)
+    # 遍历所有文件
+    for file in files:
+        file_name = os.path.splitext(file)
+        if file_name[1] == '.mp4':    # 如果后缀名为 .mp4
+            # audio = mpe.AudioFileClip(os.path.join(root, file))
+            print('当前转换文件 %s ' % file)
+            # audio.write_audiofile(os.path.join(video_path, file_name[0] + '.mp3'))
+            cmd = ffmpeg + " -i " + os.path.join(root, file) +  " -vn -sn -c:a copy -y -map 0:a:0 " + os.path.join(video_path, file_name[0] + '.aac')
+            os.system(cmd)
