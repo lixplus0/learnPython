7ab67b87b741fcade7e667b67f93064a67076a65
lixplus <lixplus@qq.com>
2019-08-21 21:04:01 +0800

    2019-08-21 21:04:00

--- Crawler/getWebsite.py
+++ Crawler/getWebsite.py
@@ -3,18 +3,47 @@ import os
 import time
 import re
 import requests
-from bs4 import BeautifulSoup
+import json
+
+json_file = "./driver.json"
+
+def load_file():
+    if os.path.exists(json_file):
+        with open(json_file, mode='r', encoding='utf-8') as f:
+            return json.load(f)
+    else:
+        with open(json_file, mode='w', encoding='utf-8') as f:
+            json.dump({}, f)
+            return {}
+
+def write_file(jsons):
+    with open(json_file, mode='w', encoding='utf-8') as f:
+        json.dump(jsons, f)
+
 
 if __name__ == "__main__":
-    WEBSITE = 'https://think.lenovo.com.cn/support/driver/newdriversdownlist.aspx?categoryid=3110338&CODEName=20KH&SearchType=1&wherePage=1&SearchNodeCC=&needmt=20KH'
+    WEBSITE = 'https://newsupport.lenovo.com.cn/api/drive/drive_listnew?searchKey=3110338&sysid=138'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
-    html_codes = 'utf8'
-
+    
     try:
         res_sub = requests.get(WEBSITE, headers=headers)
-        res_sub.encoding = html_codes
-        soup_sub = BeautifulSoup(res_sub.text, 'html.parser')
-        print(res_sub.text)
+        info_json = json.loads(res_sub.content)['data']
+        old_info = load_file()
+        print(old_info)
+        # notebook_series = info_json['driverSerious'][0]['NodeCode']
+        # notebook_pic = info_json['driverSerious'][0]['PicturePath']
+        # part_list = info_json['partList']
+        # for part in part_list:
+        #     part_name = part['PartName']
+        #     drive_list = part['drivelist']
+        #     for drive in drive_list:
+        #         drive_name = drive['DriverName']
+        #         pub_date = drive['DriverIssuedDateTime']
+        #         drive_version = drive['Version']
+        #         file_path = drive['FilePath']
+        #         print(drive_name + drive_version + pub_date)
+
+            
     except:
         print('error')
